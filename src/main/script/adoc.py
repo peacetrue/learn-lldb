01c73654cfa80ef8d779996afc140d198b8b2765
peacetrue <xiayouxue@gmail.com>
2023-07-10 13:04:20 +0800

    生成 man 手册

--- src/main/script/adoc.py
+++ src/main/script/adoc.py
@@ -4,10 +4,12 @@ import re
 import logging
 import os
 
+# 基于 Makefile 的目录
 man_dir: str = "../../docs/antora/modules/ROOT/pages/man"
 suffix: str = ".adoc"
 
 if not os.path.exists(man_dir):
+    # 自动创建 man_dir 目录
     os.makedirs(man_dir)
 
 # 配置日志输出格式
@@ -22,19 +24,9 @@ logging.basicConfig(
 
 
 def __lldb_init_module(debugger, internal_dict):
+    # lldb 初始执行
     save_command_help(debugger.GetCommandInterpreter())
-    generate_nav()
-
-
-def generate_nav():
-    navs: list[str] = []
-    file_list = os.listdir(man_dir)
-    file_list.sort()
-    for file_name in file_list:
-        commands = file_name.rstrip(".adoc").split("-")
-        navs.append(f"*{'*' * len(commands)} xref:man/{file_name}[{commands[-1]}]")
-    with open(f"{man_dir}/../../nav.adoc", "a") as file:
-        file.write("\n".join(navs))
+    generate_nav_man()
 
 
 def save_command_help(interpreter, command="help"):
@@ -42,22 +34,26 @@ def save_command_help(interpreter, command="help"):
     logging.info(f"save_command_help: {command}")
     content = handle_return_command(interpreter, command)
     logging.debug(f"man content: \n{content}")
-    # 忽略简写
-    if f"'{command.lstrip('help').strip()}' is an abbreviation for" in content:
-        return
+    # 忽略简写的命令
+    # if f"'{command.lstrip('help').strip()}' is an abbreviation for" in content:
+    #     return
     save(command, content)
     sub_commands = resolve_command_content(command, content)
+    # 删除 help help 否则 help help 的内容覆盖 help
+    sub_commands = [item for item in sub_commands if command == "help"]
     logging.info(f"sub_commands: {sub_commands}")
     for sub_command in sub_commands:
         save_command_help(interpreter, f"{command} {sub_command}")
 
 
 def resolve_command_content(command, content) -> list[str]:
+    # 解析子命令
     matches = re.findall(r"^\s+(\w+)\s+--", content, re.MULTILINE)
     return [match.strip() for match in matches]
 
 
 def save(command, content):
+    # 保存命令帮助信息
     command = command.strip().split()
     # 如果不是 help 命令，则移除前缀 help
     if len(command) > 1:
@@ -73,3 +69,15 @@ def handle_return_command(interpreter, command) -> str:
     result = lldb.SBCommandReturnObject()
     interpreter.HandleCommand(command, result)
     return result.GetOutput().rstrip("\n")
+
+
+def generate_nav_man():
+    # 生成导航中的 man 部分
+    navs: list[str] = ["* https://lldb.llvm.org/man/lldb.html[man 手册^]"]
+    file_list = os.listdir(man_dir)
+    file_list = sorted(file_list, key=lambda x: x.split('.')[0])
+    for file_name in file_list:
+        commands = file_name.split(".")[0].split("-")
+        navs.append(f"*{'*' * len(commands)} xref:man/{file_name}[{commands[-1]}]")
+    with open(f"{man_dir}/../../partials/nav-man.adoc", "w") as file:
+        file.write("\n".join(navs))
