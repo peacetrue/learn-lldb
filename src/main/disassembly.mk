f084aa1106ade179b9cbf8624f36794fc5c53736
peacetrue <xiayouxue@gmail.com>
2023-07-24 23:10:49 +0800

    同时调试源码和汇编代码

--- src/main/disassembly.mk
+++ src/main/disassembly.mk
@@ -1,7 +1,13 @@
-# 在源码调试模式和汇编调试模式之间转换
-# debug mode between source code and disassembly = dmbsd
-# https://stackoverflow.com/questions/33333165/how-to-switch-between-source-code-debug-mode-and-disassembly-debug-mode-in-lldb
+# 同时调试源码和汇编代码
 
 # error: invalid enumeration value '1', valid values are: never, always, no-debuginfo, no-source
-disassembly.case: $(BUILD)/disassembly.bin
-	lldb -b -s source/disassembly.source $<
+# 没有调试信息时，按汇编调试
+disassembly.nodebug.case: CFLAGS:=
+disassembly.nodebug.case: clean/disassembly.bin lldb/disassembly
+
+# 有调试信息时，按源码调试
+disassembly.debug.case: clean/disassembly.bin lldb/disassembly
+
+# 有调试信息时，同时按源码和汇编调试
+disassembly.case: clean/disassembly.bin $(BUILD)/disassembly.bin
+	lldb -b -s source/disassembly.source $(word 2,$^)
